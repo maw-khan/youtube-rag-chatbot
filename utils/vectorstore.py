f8838381f5d39d02ddfe7453b6d517d6bd2c3383
Muhammad Ali Waris Khan <MAWKHAN5031@GMAIL.COM>
2026-05-18 21:29:02 +0500

    Create vectorstore.py

---
+++ utils/vectorstore.py
@@ -0,0 +1,44 @@
+import os
+
+from langchain.vectorstores import (
+    FAISS
+)
+
+
+VECTOR_DB_PATH = "data/faiss_index"
+
+
+def create_vector_store(
+    documents,
+    embedding_model
+):
+
+    vector_store = FAISS.from_documents(
+        documents,
+        embedding_model
+    )
+
+    vector_store.save_local(
+        VECTOR_DB_PATH
+    )
+
+    return vector_store
+
+
+def load_vector_store(
+    embedding_model
+):
+
+    if os.path.exists(
+        VECTOR_DB_PATH
+    ):
+
+        vector_store = FAISS.load_local(
+            VECTOR_DB_PATH,
+            embedding_model,
+            allow_dangerous_deserialization=True
+        )
+
+        return vector_store
+
+    return None
